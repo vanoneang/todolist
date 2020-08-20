67bf11d082f4678f739a9ab2f7d92a3ff0c70e9e
vanoneang <525650856@qq.com>
2020-08-20 09:30:40 +0800

    add prefetch

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,17 +3,31 @@ import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './home.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Link to={'/todo'}>
-          <p  className="yellow">跳转TODOLIST页</p>
-        </Link>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to={'/todo'}>
+            <p  className="yellow">跳转TODOLIST页</p>
+          </Link>
+        </header>
+      </div>
+    )
+  };
+
+  componentDidMount() {
+    
+    // eslint-disable-next-line no-unused-expressions
+    import(
+      /* webpackPrefetch: true */
+      /* webpackChunkName: 'todo' */
+      "../todolist"
+    )
+
+  }
 }
+
 
 export default App;
